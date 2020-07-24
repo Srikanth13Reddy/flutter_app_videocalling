f55241631889ac3945e957a055ffa9a4fd9cb7b8
Srikanth13Reddy <srikanthy@apptomate.co>
2020-07-24 12:40:50 +0530

    Initial commit

---
+++ android/app/src/main/java/com/apptomate/flutter_app_videocalling/MainActivity.java
@@ -0,0 +1,6 @@
+package com.apptomate.flutter_app_videocalling;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,2 @@
+// Agora AppId
+const APP_ID = '936e149d8b4c45c8941ee20a4c715add';
